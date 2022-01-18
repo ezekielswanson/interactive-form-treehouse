25dbfcebfe52b96274d8d8b5bcdcae6195862053
ezekielswanson <swansonezekiel@gmail.com>
2022-01-17 19:28:49 -0600

    Updated js file.

--- script.js
+++ script.js
@@ -10,8 +10,9 @@ const otherJobRoleInpt = document.querySelector('.other-job-role')
 
 /* Variables for the T-Shirt Info section */
 
-const tShirtColor = document.querySelector('#color');
 const shirtDesign = document.querySelector('#design');
+const tShirtColor = document.querySelector('#color');
+
 
 
 
@@ -35,14 +36,27 @@ jobRole.addEventListener('change', e => {
 
 
 /* Disables Color input on page load*/
-tShirtColor.disabled = true;
+
+tShirtColor[0].disabled = true;
+
 
 /* This function enables the color input once a t shirt design is selected */
 shirtDesign.addEventListener('change', (e) => {
-    tShirtColor.disable = false;
+    tShirtColor[0].disabled= false;
+    const tShirtColorOpt = tShirtColor.children;
 
-    if (e.target.value = "js puns") {
-        
+    for (let i = 0; i < tShirtColorOpt.length; i++) {
+        tShirtOptValue = e.target.value;
+        tShirtTheme = tShirtColorOpt.getAttribute('data-theme');
+
+        if (tShirtOptValue ===  tShirtTheme) {
+            tShirtColorOpt[i].hidden = true;
+        }
+
+        else {
+            tShirtColorOpt[i].hidden = false;
+        }
+
     }
     
 });
